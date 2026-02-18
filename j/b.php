23bba4ecaffdbb98237ccda9831d93eba9386f56
65010912541 <65010912541@msu.ac.th>
2026-02-18 16:35:54 +0700

    Add files via upload

---
+++ j/b.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชัชวาล สิงห์เทศ(แบงค์)</title>
+</head>
+
+<body>
+<h1>ชัชวาล สิงห์เทศ(แบงค์)</h1>
+
+<img src="img/1.jpg" width="200" >
+</body>
+</html>
+
